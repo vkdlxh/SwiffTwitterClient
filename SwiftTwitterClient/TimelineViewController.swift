0f639c24be4f6b1c5f28f7629b91c3f9e1d2ff18
jeon sangjun <tkdwnszoq@naver.com>
2017-10-20 21:30:44 +0900

    DummyData

--- SwiftTwitterClient/TimelineViewController.swift
+++ SwiftTwitterClient/TimelineViewController.swift
@@ -10,36 +10,61 @@ import UIKit
 
 class TimelineViewController: UIViewController {
 
-    @IBOutlet weak var tableView: UITableView!
     
+    // MARK: Properties
     // テーブル表示用のデータソース
     var tweets: [Tweet] = []
     
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let screenName: [String] = ["abcde", "12345", "Korea", "Japan", "SwiftPractice"]
+        let name: [String] = ["홍길동", "신짱구", "김철수", "김영희", "맹구"]
+        let profileImageURL: [String] = ["https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg",
+                                         "https://pbs.twimg.com/media/COmjW9LUkAA21g8.jpg",
+                                         "https://i.imgur.com/FTBSFo7.png",
+                                         "https://cdn.theatlantic.com/assets/media/img/photo/2015/11/images-from-the-2016-sony-world-pho/s01_130921474920553591/main_900.jpg?1448476701",
+                                         "https://static1.squarespace.com/static/524263b4e4b0adb01903b572/t/575edefe86db433ce0efcf9b/1465835270342/",
+                                         ]
+        
         // ダーミデータの生成
-        let user = User(id: "1", screenName: "tkdwnszoq", name: "전상준", profileImageURL: "https://pbs.twimg.com/media/COmjW9LUkAA21g8.jpg")
-        let tweet = Tweet(id: "01", text: "Twitterを作ってみましょう。", user: user)
+        for _ in 0...5 {
         
-        let tweets = [tweet]
-        self.tweets = tweets
+            let user = User(id: "1",
+                            screenName: "\(screenName.randomItem() ?? "")",
+                            name: "\(name.randomItem() ?? "")",
+                            profileImageURL: "\(profileImageURL.randomItem() ?? "")")
+            let tweet = Tweet(id: "01", text: "Twitterを作ってみましょう。ここは本文です。", user: user)
         
+            //let tweets = [tweet]
+            self.tweets.append(tweet)
+        }
         // tableViewのリロード
         tableView.reloadData()
     }
 }
 
+// MARK: Extensions
 extension TimelineViewController: UITableViewDelegate {
     
     // セルがタップされたのを検知したときに実行する処理
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //TODO: あとで
         print("セルがタップされました。")
     }
     
     // --- UITableViewDelegateはセルタップなどのユーザーイベント他にも、UITableViewの見た目や挙動に関する機能も含まれています。---
     // セルの見積もりお高さを指定する処理
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        // FIXME: ここ修正必要だ！
         return 200
     }
     
@@ -48,6 +73,7 @@ extension TimelineViewController: UITableViewDelegate {
         return UITableViewAutomaticDimension
     }
 }
+
 
 extension TimelineViewController: UITableViewDataSource {
     
@@ -66,5 +92,13 @@ extension TimelineViewController: UITableViewDataSource {
         cell.fill(tweet: tweets[indexPath.row])
         
         return cell
+    }
+}
+
+extension Array {
+    func randomItem() -> Element? {
+        if isEmpty { return nil }
+        let index = Int(arc4random_uniform(UInt32(self.count)))
+        return self[index]
     }
 }
